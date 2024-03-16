ecb334c5185cf4d4d07661c778f6e60d63339781
Tenshihiko <arai.tenshi@gmail.com>
2024-03-16 16:59:51 +0100

    60; 100209

---
+++ LeetCode/LeetCode100209.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode100209
+{
+    public class Solution
+    {
+
+        public void Run()
+        {
+            Console.WriteLine(UnmarkedSumArray(
+                [1, 2, 2, 1, 2, 3, 1],
+[[1, 2], [3, 3], [4, 2]]));
+        }
+        public long[] UnmarkedSumArray(int[] nums, int[][] queries)
+        {
+            var sum = nums.Sum();
+
+            var res = new long[queries.Length];
+
+            var marked = new bool[nums.Length];
+
+            var q = new PriorityQueue<int, (int, int)>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                q.Enqueue(i, (nums[i], i));
+            }
+
+            for (int i = 0; i < queries.Length; i++)
+            {
+
+                if (!marked[queries[i][0]])
+                {
+                    marked[queries[i][0]] = true;
+                    sum -= nums[queries[i][0]];
+                }
+
+                var k = queries[i][1];
+
+                while (k > 0 && q.Count > 0)
+                {
+                    var ind = q.Dequeue();
+                    if (!marked[ind])
+                    {
+                        marked[ind] = true;
+                        k--;
+                        sum -= nums[ind];
+                    }
+                }
+
+                res[i] = sum;
+            }
+
+            return res;
+        }
+    }
+}
---
+++ LeetCode/LeetCode60.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode60
+{
+    public class Solution
+    { 
+        public void Run()
+        {
+           Console.WriteLine(GetPermutation(3, 3) == "213");
+           Console.WriteLine(GetPermutation(4, 9) == "2314");
+           Console.WriteLine(GetPermutation(3, 1) == "123");
+           Console.WriteLine(GetPermutation(1, 1) == "1");
+        }
+
+        public string GetPermutation(int n, int k)
+        {
+            if (n == 1 && k == 1) return "1";
+
+            var res = "";
+            var used = new bool[n];
+
+            var nfact = Enumerable.Range(1, n - 1).Aggregate(1, (p, item) => p * item);
+
+            for (int i = 0; i < n; i++)
+            {
+                var x = k / nfact;
+
+                while (used[x])
+                {
+                    x = (x + 1) % n;
+                }
+
+                used[x] = true;
+                res += (x + 1);
+                k %= nfact;
+                nfact  = i + 1 < n ? nfact / (n - i - 1) : 1;
+            }
+
+            return res;
+        }
+    }
+}
