65ae7a7a97f530564d5f59ff841e988fce73cbab
Tenshihiko <arai.tenshi@gmail.com>
2024-03-29 12:50:09 +0100

    5 mode tasks

---
+++ LeetCode/LeetCode143.cs
@@ -0,0 +1,57 @@
+﻿using LeetCode.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static LeetCode1171.Solution;
+
+namespace LeetCode143
+{
+    public class Solution
+    {
+        public void Run()
+        {
+            ReorderList(ListNode.ListFromArray([1, 2, 3, 4, 5]));
+        }
+
+        public ListNode Middle(ListNode head)
+        {
+            var root = new ListNode(0, head);
+            var slow = root;
+            var fast = root;
+
+            while (fast != null)
+            {
+                fast = fast.next?.next;
+                slow = slow.next;
+            }
+
+            return slow;
+        }
+
+        public void ReorderList(ListNode head)
+        {
+            var middle = Middle(head);
+
+            var stack = new Stack<ListNode>();
+
+            while (middle != null)
+            {
+                stack.Push(middle);
+                middle = middle.next;
+            }
+
+            var curr = head;
+            while (stack.Count > 0 && curr != null)
+            {
+                var next = curr.next;
+                curr.next = stack.Pop();
+                curr.next.next = next;
+                curr = next;
+            }
+
+            //return head;
+        }
+    }
+}
---
+++ LeetCode/LeetCode525.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode525
+{
+    public class Solution
+    {
+        public void Run()
+        {
+            Console.WriteLine(FindMaxLength([0, 1, 0]));
+        }
+
+        public int FindMaxLength(int[] nums)
+        {
+
+            var prevzeros = new int[nums.Length];
+            var prevones = new int[nums.Length];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                prevzeros[i] = (nums[i] == 0 ? 1 : 0)
+                        + (i == 0 ? 0 : prevzeros[i - 1]);
+                prevones[i] = (nums[i] == 1 ? 1 : 0)
+                        + (i == 0 ? 0 : prevones[i - 1]);
+            }
+
+            var max = 0;
+
+            for (int i = -1; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    var zeros = prevzeros[j] - (i == -1 ? 0 : prevzeros[i]);
+                    var ones = prevones[j] - (i == -1 ? 0 : prevones[i]);
+
+                    if (zeros == ones)
+                    {
+                        max = Math.Max(max, j - i);
+                    }
+                }
+            }
+
+            return max;
+        }
+    }
+}
