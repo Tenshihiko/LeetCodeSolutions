5d93e53e119e4fbb9e92f9b82f96414333952bd5
Tenshihiko <arai.tenshi@gmail.com>
2025-09-17 13:34:05 +0200

    2197, 2353

---
+++ LeetCode/2025/LeetCode2197.cs
@@ -0,0 +1,49 @@
+﻿namespace LeetCode2197;
+
+public class Solution
+{
+    public void Run()
+    {
+        // var param = ...;
+        // var result = YourMethod(param);
+    }
+
+    public int[] ReplaceNonCoprimes(int[] nums)
+    {
+        var stack = new Stack<int>();
+        foreach (var num in nums)
+        {
+            stack.Push(num);
+            while (stack.Count > 1)
+            {
+                var a = stack.Pop();
+                var b = stack.Pop();
+                var gcd = GCD(a, b);
+                if (gcd == 1)
+                {
+                    stack.Push(b);
+                    stack.Push(a);
+                    break;
+                }
+                else
+                {
+                    var lcm = a / gcd * b;
+                    stack.Push(lcm);
+                }
+            }
+        }
+        return stack.Reverse().ToArray();
+    }
+
+    private int GCD(int a, int b)
+    {
+        while (b != 0)
+        {
+            var temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
+
---
+++ LeetCode/2025/LeetCode2353.cs
@@ -0,0 +1,57 @@
+﻿namespace LeetCode2353;
+
+public class Solution
+{
+    public void Run()
+    {
+        // var param = ...;
+        // var result = YourMethod(param);
+    }
+
+    public class FoodRatings
+    {
+
+        Dictionary<string, SortedSet<(int rating, string food)>> cuisineMap = new Dictionary<string, SortedSet<(int rating, string food)>>();
+        Dictionary<string, (string cuisine, int rating)> foodMap = new Dictionary<string, (string cuisine, int rating)>();
+
+        public FoodRatings(string[] foods, string[] cuisines, int[] ratings)
+        {
+            for (int i = 0; i < foods.Length; i++)
+            {
+                var food = foods[i];
+                var cuisine = cuisines[i];
+                var rating = ratings[i];
+                if (!cuisineMap.ContainsKey(cuisine))
+                {
+                    cuisineMap[cuisine] = new SortedSet<(int rating, string food)>(Comparer<(int rating, string food)>.Create((a, b) =>
+                    {
+                        if (a.rating != b.rating)
+                        {
+                            return b.rating.CompareTo(a.rating); // Descending order by rating
+                        }
+                        return a.food.CompareTo(b.food); // Ascending order by food name
+                    }));
+                }
+                cuisineMap[cuisine].Add((rating, food));
+                foodMap[food] = (cuisine, rating);
+            }
+        }
+
+        public void ChangeRating(string food, int newRating)
+        {
+            if (foodMap.ContainsKey(food))
+            {
+                var (cuisine, oldRating) = foodMap[food];
+                cuisineMap[cuisine].Remove((oldRating, food));
+                cuisineMap[cuisine].Add((newRating, food));
+                foodMap[food] = (cuisine, newRating);
+            }
+        }
+
+        public string HighestRated(string cuisine)
+        {
+            return cuisineMap[cuisine].First().food;
+        }
+    }
+}
+
