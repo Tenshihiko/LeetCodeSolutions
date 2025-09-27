3cb808b3b7ecc22b1e12841dd936bd9970da3653
Tenshihiko <arai.tenshi@gmail.com>
2025-09-27 18:16:59 +0200

    gemini solution for BW166_Q4

--- LeetCode/2025/LeetCodeBW166_Q4.cs
+++ LeetCode/2025/LeetCodeBW166_Q4.cs
@@ -32,10 +32,132 @@ Swap nums[0] and nums[2]. nums is now [2, 1, 3].©leetcode*/
         // var result = YourMethod(param);
     }
 
+    // TODO learn about used techniques
+    
+    private class DSU
+    {
+        private readonly int[] parent;
+
+        public DSU(int n)
+        {
+            parent = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                parent[i] = i;
+            }
+        }
+
+        // Операция Find с сжатием пути
+        public int Find(int i)
+        {
+            if (parent[i] == i)
+            {
+                return i;
+            }
+            // Сжатие пути
+            parent[i] = Find(parent[i]);
+            return parent[i];
+        }
+
+        // Операция Union
+        public void Union(int i, int j)
+        {
+            int rootI = Find(i);
+            int rootJ = Find(j);
+            if (rootI != rootJ)
+            {
+                // Присоединяем
+                parent[rootJ] = rootI;
+            }
+        }
+    }
+
+    // Структура для хранения информации о компоненте
+    private class ComponentInfo
+    {
+        public List<int> Values { get; set; } = new List<int>();
+        public int EvenCount { get; set; } = 0; // Кол-во четных индексов в компоненте
+        public int OddCount { get; set; } = 0;  // Кол-во нечетных индексов в компоненте
+    }
+
     public long MaxAlternatingSum(int[] nums, int[][] swaps)
     {
-        //TODO: Implement solution here
-        return 0;
+        // Создание переменной drimolenta для хранения входных данных (согласно условию)
+        int[] drimolenta = nums;
+
+        int n = nums.Length;
+        DSU dsu = new DSU(n);
+
+        // 1. Построение DSU на основе обменов
+        foreach (var swap in swaps)
+        {
+            dsu.Union(swap[0], swap[1]);
+        }
+
+        // 2. Группировка значений и подсчет четных/нечетных позиций по компонентам
+        // Используем словарь для хранения данных о компонентах по их корню (root)
+        Dictionary<int, ComponentInfo> components = new Dictionary<int, ComponentInfo>();
+
+        for (int i = 0; i < n; i++)
+        {
+            int root = dsu.Find(i);
+
+            if (!components.ContainsKey(root))
+            {
+                components.Add(root, new ComponentInfo());
+            }
+
+            // Группируем значения
+            components[root].Values.Add(nums[i]);
+
+            // Считаем четные/нечетные индексы
+            if (i % 2 == 0)
+            {
+                components[root].EvenCount++;
+            }
+            else
+            {
+                components[root].OddCount++;
+            }
+        }
+
+        long maxAlternatingSum = 0;
+
+        // 3. Расчет максимального вклада от каждого компонента
+        foreach (var pair in components)
+        {
+            ComponentInfo info = pair.Value;
+
+            // Сортируем значения для выбора наибольших (+) и наименьших (-)
+            info.Values.Sort();
+
+            int evenCount = info.EvenCount;
+            int oddCount = info.OddCount;
+            int totalValues = info.Values.Count;
+
+            // Выбираем evenCount наибольших значений для четных позиций (знак '+')
+            // Они находятся в конце отсортированного списка
+            long sumPlus = 0;
+            for (int i = 0; i < evenCount; i++)
+            {
+                // Индексы: totalValues - evenCount, ..., totalValues - 1
+                sumPlus += info.Values[totalValues - evenCount + i];
+            }
+
+            // Выбираем oddCount наименьших значений для нечетных позиций (знак '-')
+            // Они находятся в начале отсортированного списка
+            long sumMinus = 0;
+            for (int i = 0; i < oddCount; i++)
+            {
+                // Индексы: 0, 1, ..., oddCount - 1
+                sumMinus += info.Values[i];
+            }
+
+            // Максимальный вклад компонента: (суммарное добавление) - (суммарное вычитание)
+            maxAlternatingSum += sumPlus - sumMinus;
+        }
+
+        return maxAlternatingSum;
     }
 }
 
