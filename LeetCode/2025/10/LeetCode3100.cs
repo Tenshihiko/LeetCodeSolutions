d3c187f678f1732fd9331ae88854d168b4d4d184
Tenshihiko <arai.tenshi@gmail.com>
2025-10-02 11:25:20 +0200

    3100

---
+++ LeetCode/2025/10/LeetCode3100.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCode3100;
+
+public class Solution
+{
+    public void Run()
+    {
+        // var param = ...;
+        var result = MaxBottlesDrunk(10, 6);
+    }
+
+    public int MaxBottlesDrunk(int numBottles, int numExchange)
+    {
+        var drunk = 0;
+        var full = numBottles;
+        var empty = 0;
+
+        while (full > 0)
+        {
+
+            drunk += full;
+            empty += full;
+            full = 0;
+
+            if (empty < numExchange)
+            {
+                break;
+            }
+
+            while (empty >= numExchange)
+            {
+                empty -= numExchange;
+                numExchange++;
+                full++;
+            }
+        }
+
+        return drunk;
+    }
+}
+
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,6 +1,6 @@
 ﻿
 
-using LeetCode1039;
+using LeetCode3100;
 
 var solution = new Solution();
 solution.Run();
